180a4c0d2134b6385021417c50238cc4e01beb0a
Mera Youssef <merayoussef91@hotmail.com>
2020-09-12 19:48:12 -0500

    working on adding 5 day forecast

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -9,30 +9,42 @@ var getCityInput = function(cityName) {
           .catch(function() {
             // catch any errors
           })
-        
+
 }
 
 window.onload = function() {
     getCityInput("Nashville");
         }
-    
+
+        // add instead of Nashville
+var search = document.querySelector('user-input')
+
 var displayCurrentWeather = function(degrees) {
-    var celcius = Math.round(parseFloat(degrees.main.temp)-273.15);
     var fahrenheit = Math.round(((parseFloat(degrees.main.temp)-273.15)*1.8)+32);
-    var description = degrees.weather[0].description;
-
-    document.getElementById('description').innerHTML = degrees.weather[0].description;
-    document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
-	document.getElementById('location').innerHTML = degrees.name;
-
-    if( description.indexOf('rain') > 0 ) {
-        document.body.className = 'rainy';
-    } else if( description.indexOf('cloud') > 0 ) {
-        document.body.className = 'cloudy';
-    } else if( description.indexOf('sunny') > 0 ) {
-        document.body.className = 'sunny';
-    }
+    
+    document.getElementById('location').innerHTML = degrees.name;
+    document.getElementById('temp').innerHTML = "Tempature: " + fahrenheit + '&deg;' + "F";
+    document.getElementById('humidity').innerHTML = "Humidity: " + degrees.main.humidity + '%';
+    document.getElementById('wind').innerHTML = "Wind Speed: " + degrees.wind.speed + " MPH";
+   
 }
+
+var uvIndexInfo = function () {
+    var apiKey = 'ef81a5dadc206fb285c8563fe1675b51';
+    fetch('http://api.openweathermap.org/data/2.5/uvi?' + '&appid=' + apiKey + '&lat=36.7' + '&lon=-86.78')
+    .then(function(response){
+    return response.json() })
+    .then(function() {
+        uvIndex(data);
+      })
+      .catch(function() {
+        // catch any errors
+      })
+}
+var uvIndex = function() {
+    document.getElementById('uv-index').innerHTML = "UV Index: " + value;
+}
+
 
 var fiveDayForecast = function() {
 var apiKey = 'ef81a5dadc206fb285c8563fe1675b51';
@@ -50,15 +62,11 @@ var apiKey = 'ef81a5dadc206fb285c8563fe1675b51';
 fiveDayForecast()
 
 var display5DayForecast = function() {
-    document.getElementById('description5day').innerHTML = degrees.weather[0].description;
-    document.getElementById('temp5day').innerHTML = fahrenheit + '&deg;';
-	document.getElementById('location5day').innerHTML = degrees.name;
-
-    if( description.indexOf('rain') > 0 ) {
-        document.body.className = 'rainy';
-    } else if( description.indexOf('cloud') > 0 ) {
-        document.body.className = 'cloudy';
-    } else if( description.indexOf('sunny') > 0 ) {
-        document.body.className = 'sunny';
-    }
+    var fahrenheit = Math.round(((parseFloat(list.main.temp)-273.15)*1.8)+32)
+    document.getElementById('date-5').innerHTML = list.dt;
+    document.getElementById('icon-5').innerHTML = weather.icon;
+    document.getElementById('temp-5').innerHTML = "Tempature: " + fahrenheit + '&deg;' + "F";
+	document.getElementById('hum-5').innerHTML = "Humidity: " + list.main.humidity;
+console.log('data-5')
 }
+
