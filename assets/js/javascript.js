7385c67eba0ccf02e7460d4cb2c4da1e8e5bf8aa
Mera Youssef <merayoussef91@hotmail.com>
2020-09-12 21:59:43 -0500

    updated style and added api info for 5 day forecast

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,72 +1,98 @@
-var getCityInput = function(cityName) {
+var getCityInput = function (cityName) {
     var apiKey = 'ef81a5dadc206fb285c8563fe1675b51';
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName+ '&appid=' + apiKey)
-    .then(function(response){
-        return response.json() })
-        .then(function(data) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
             displayCurrentWeather(data);
-          })
-          .catch(function() {
+        })
+        .catch(function () {
             // catch any errors
-          })
+        })
 
 }
 
-window.onload = function() {
-    getCityInput("Nashville");
-        }
+getCityInput("Nashville");
 
-        // add instead of Nashville
+// add instead of Nashville
 var search = document.querySelector('user-input')
 
-var displayCurrentWeather = function(degrees) {
-    var fahrenheit = Math.round(((parseFloat(degrees.main.temp)-273.15)*1.8)+32);
-    
+var displayCurrentWeather = function (degrees) {
+    var fahrenheit = Math.round(((parseFloat(degrees.main.temp) - 273.15) * 1.8) + 32);
+
     document.getElementById('location').innerHTML = degrees.name;
     document.getElementById('temp').innerHTML = "Tempature: " + fahrenheit + '&deg;' + "F";
     document.getElementById('humidity').innerHTML = "Humidity: " + degrees.main.humidity + '%';
     document.getElementById('wind').innerHTML = "Wind Speed: " + degrees.wind.speed + " MPH";
-   
+
 }
 
 var uvIndexInfo = function () {
     var apiKey = 'ef81a5dadc206fb285c8563fe1675b51';
     fetch('http://api.openweathermap.org/data/2.5/uvi?' + '&appid=' + apiKey + '&lat=36.7' + '&lon=-86.78')
-    .then(function(response){
-    return response.json() })
-    .then(function() {
-        uvIndex(data);
-      })
-      .catch(function() {
-        // catch any errors
-      })
-}
-var uvIndex = function() {
-    document.getElementById('uv-index').innerHTML = "UV Index: " + value;
-}
-
-
-var fiveDayForecast = function() {
-var apiKey = 'ef81a5dadc206fb285c8563fe1675b51';
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + 'Nashville' + '&appid=' + apiKey)
-    .then(function(response){
-        return response.json() })
-        .then(function(data) {
-            display5DayForecast(data);
-          })
-          .catch(function() {
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (index) {
+            uvIndex(index);
+        })
+        .catch(function () {
             // catch any errors
-          })
+        })
+}
+uvIndexInfo()
+
+var uvIndex = function (index) {
+    document.getElementById('uv-index').innerHTML = "UV Index: " + index.value;
+}
+
+
+
+var fiveDayForecast = function () {
+    var apiKey = 'ef81a5dadc206fb285c8563fe1675b51';
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + 'Nashville' + '&appid=' + apiKey)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            display5DayForecast(data);
+        })
+        .catch(function () {
+            // catch any errors
+        })
 
 }
 fiveDayForecast()
 
-var display5DayForecast = function() {
-    var fahrenheit = Math.round(((parseFloat(list.main.temp)-273.15)*1.8)+32)
-    document.getElementById('date-5').innerHTML = list.dt;
-    document.getElementById('icon-5').innerHTML = weather.icon;
-    document.getElementById('temp-5').innerHTML = "Tempature: " + fahrenheit + '&deg;' + "F";
-	document.getElementById('hum-5').innerHTML = "Humidity: " + list.main.humidity;
-console.log('data-5')
+var display5DayForecast = function (data) {
+    console.log(data)
+    var fahrenheit = Math.round(((parseFloat(data.list[0].main.temp) - 273.15) * 1.8) + 32)
+
+    document.getElementById('five-day-date-1').innerHTML = data.list[0].dt_txt;
+    document.getElementById('five-day-icon-1').innerHTML = data.list[0].weather[0].icon;
+    document.getElementById('five-day-temp-1').innerHTML = "Tempature: " + fahrenheit + '&deg;' + "F";
+    document.getElementById('five-day-hum-1').innerHTML = "Humidity: " + data.list[0].main.humidity + "%";
+
+    document.getElementById('five-day-date-2').innerHTML = data.list[7].dt_txt;
+    document.getElementById('five-day-icon-2').innerHTML = data.list[7].weather[0].icon;
+    document.getElementById('five-day-temp-2').innerHTML = "Tempature: " + fahrenheit + '&deg;' + "F";
+    document.getElementById('five-day-hum-2').innerHTML = "Humidity: " + data.list[7].main.humidity + "%";
+
+    document.getElementById('five-day-date-3').innerHTML = data.list[15].dt_txt;
+    document.getElementById('five-day-icon-3').innerHTML = data.list[15].weather[0].icon;
+    document.getElementById('five-day-temp-3').innerHTML = "Tempature: " + fahrenheit + '&deg;' + "F";
+    document.getElementById('five-day-hum-3').innerHTML = "Humidity: " + data.list[15].main.humidity + "%";
+
+
+    document.getElementById('five-day-date-4').innerHTML = data.list[23].dt_txt;
+    document.getElementById('five-day-icon-4').innerHTML = data.list[23].weather[0].icon;
+    document.getElementById('five-day-temp-4').innerHTML = "Tempature: " + fahrenheit + '&deg;' + "F";
+    document.getElementById('five-day-hum-4').innerHTML = "Humidity: " + data.list[23].main.humidity + "%";
+
+    document.getElementById('five-day-date-5').innerHTML = data.list[31].dt_txt;
+    document.getElementById('five-day-icon-5').innerHTML = data.list[31].weather[0].icon;
+    document.getElementById('five-day-temp-5').innerHTML = "Tempature: " + fahrenheit + '&deg;' + "F";
+    document.getElementById('five-day-hum-5').innerHTML = "Humidity: " + data.list[31].main.humidity + "%";
 }
 
